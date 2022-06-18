b89c5b689a824233fe66088cb54c350682b99348
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-06-18 16:00:23 -0300

    choices

--- apps/wl1-python-django/pets/models.py
+++ apps/wl1-python-django/pets/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+PET_CHOICES = (("cat", "cat"), ("dog", "dog"))
+
+
 class Owner(models.Model):
     name = models.CharField(max_length=30)
     birthday = models.DateField()
@@ -10,5 +13,5 @@ class Pet(models.Model):
     owner = models.ForeignKey(Owner, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     age = models.IntegerField()
-    type = models.CharField(max_length=30)
+    type = models.CharField(max_length=30, choices=PET_CHOICES)
     breed = models.CharField(max_length=30)
