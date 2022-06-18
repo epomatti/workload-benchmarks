f29cba404156a6232697b433114895605ba8d708
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-06-18 15:45:07 -0300

    get one

--- apps/wl1-python-django/wl1_python_django/urls.py
+++ apps/wl1-python-django/wl1_python_django/urls.py
@@ -3,12 +3,11 @@ from rest_framework import routers
 from pets import views
 
 router = routers.DefaultRouter()
-router.register(r'api/owners', views.OwnerViewSet)
-router.register(r'api/pets', views.PetViewSet)
+router.register(r"api/owners", views.OwnerViewSet)
+router.register(r"api/pets", views.PetViewSet)
+router.register(r"api/pets/<int:pk>", views.PetViewSet)
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path("", include(router.urls)),
     # path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
