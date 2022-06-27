d13d72d0272b71ce4d973f34d33f076fbc60b73d
Evandro Pomatti <evandro.pomatti@gmail.com>
2022-06-27 19:20:22 -0300

    k6 fixes

--- apps/wl1-python-django/k6/plan.js
+++ apps/wl1-python-django/k6/plan.js
@@ -3,20 +3,18 @@ import { sleep } from 'k6';
 
 export default function () {
 
+  const server = "app-benchmark999.azurewebsites.net";
+
   const ownerBody = {
     "name": "John",
     "age": 16,
     "birthday": "2000-01-05"
   }
 
-  const owner = http.post("https://app-workloadbenchmark-999.azurewebsites.net/api/owners/", ownerBody);
+  const owner = http.post(`https://${server}/api/owners/`, ownerBody);
   const ownerObj = JSON.parse(owner.body);
-  http.get(`https://app-workloadbenchmark-999.azurewebsites.net/api/owners/${ownerObj.id}`);
-  console.log(ownerObj.id)
+  const res1 = http.get(`https://${server}/api/owners/${ownerObj.id}/`);
 
-  sleep(1);
-  
-  console.log(ownerObj.id)
   const petBody = {
     "name": "Linda",
     "age": 10,
@@ -24,13 +22,12 @@ export default function () {
     "breed": "fox",
     "owner": ownerObj.id
   }
-  
-  const pet = http.post("https://app-workloadbenchmark-999.azurewebsites.net/api/pets/", petBody);
-  console.log(pet.body)
-  const petObj = JSON.parse(pet.body);
-  console.log(petObj)
 
-  http.get(`https://app-workloadbenchmark-999.azurewebsites.net/api/pets/${petObj.id}`);
+  const pet = http.post(`https://${server}/api/pets/`, petBody);
+  const petObj = JSON.parse(pet.body);
+
+  http.get(`https://${server}/api/pets/${petObj.id}/`);
 
   sleep(1);
+
 }
--- apps/wl1-python-django/k6/readme.md
+++ apps/wl1-python-django/k6/readme.md
@@ -1,1 +1,3 @@
 k6 run --vus 10 --duration 30s plan.js
+
+k6 run --vus 1 --duration 1s plan.js
